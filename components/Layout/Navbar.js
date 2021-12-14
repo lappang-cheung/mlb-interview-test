2f7d339723d0f5b41c73ee4c7dd70a10eee85873
Leo Cheung <lappangcheung@gmail.com>
2021-12-14 00:38:43 -0500

    add some responsiveness

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { withRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -8,6 +8,8 @@ const Navbar = ({ router }) => {
   const getLogo = () => {
     return `https://statsapi.mlb.com/logo.94f6aeb4.png`
   }
+
+  const [toggle, setToggle] = useState(false)
 
   useEffect(() => {
     getLogo()
@@ -21,17 +23,24 @@ const Navbar = ({ router }) => {
             <img src={getLogo()} width={"100%"} height={"100%"} />
           </a>
         </Link>
+        <a role="button" className={toggle ? "navbar-burger is-active": "navbar-burger"} 
+           aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" 
+           onClick={() => setToggle(!toggle)}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navbarBasicExample" className={toggle ? "navbar-menu is-active": "navbar-menu"}>
         <div className="navbar-end">
           <Link href="/">
-            <a className="navbar-item">
+            <a className="navbar-item" onClick={() => setToggle(false)}>
               Home
             </a>
           </Link>
           <Link href="/about">
-            <a className="navbar-item">
+            <a className="navbar-item" onClick={() => setToggle(false)}>
               About
             </a>
           </Link>
